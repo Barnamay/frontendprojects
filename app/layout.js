38745cadc74bcffec80e8a15d8d6ccdd89207617
BARNAMAY CHOWDHURY <143214000+Barnamay@users.noreply.github.com>
2025-07-11 07:24:03 +0530

    Update layout.js

---
+++ app/layout.js
@@ -0,0 +1,13 @@
+
+export const metadata = {
+  title: 'My Single Page',
+  description: 'Generated with Next.js and Tailwind',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
